8238fa41c9391330f54b63989c7b9eaafc8a87be
MarceloMSilveira <marcelomendes.silveira@gmail.com>
2025-07-10 14:25:32 -0300

    init

--- src/ms1token_frontend/src/App.jsx
+++ src/ms1token_frontend/src/App.jsx
@@ -1,30 +1,17 @@
-import { useState } from 'react';
-import { ms1token_backend } from 'declarations/ms1token_backend';
+import Header from "./components/Header";
+import Faucet from "./components/Faucet";
+import Balance from "./components/Balance";
+import Transfer from "./components/Transfer";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    ms1token_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <div id="screen">
+      <Header />
+      <Faucet />
+      <Balance />
+      <Transfer />
+    </div>
   );
 }
 
